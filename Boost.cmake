88df595956f3d1c3573b979fd4ee9ba6e5e020b3
parnell <leeparnell@gmail.com>
2018-04-08 08:27:32 -0500

    Revised make for tacc

--- Boost.cmake
+++ Boost.cmake
@@ -1,6 +1,8 @@
 
 cmake_minimum_required(VERSION 2.8)
 set(Boost_DEBUG ON)
+
+#set(BOOST_ROOT "$ENV{HOME}/usr/local/boost-1.66.0" CACHE PATH "/home1/01208/parnell/usr/local/boost-1.66.0" )
 find_package(Boost COMPONENTS serialization mpi REQUIRED )
 # find with required parts find_package(Boost REQUIRED mpi serialization)
 
